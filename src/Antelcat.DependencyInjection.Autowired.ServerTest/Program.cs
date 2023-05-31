d7d1f6081e7e398320ca085d75ca597f4bb98b37
Feast <feast107@gmail.com>
2023-05-31 15:23:55 +0800

    Update publish

--- src/Antelcat.DependencyInjection.Autowired.ServerTest/Program.cs
+++ src/Antelcat.DependencyInjection.Autowired.ServerTest/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers().AddControllersAsServices().UseAutowiredControl
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseAutowiredServiceProviderFactory();
 
 var app = builder.Build();
 
